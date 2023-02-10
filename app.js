5675f2e3861ded10a240b5e052a85382cdf3e4a1
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-10 16:38:04 +0530

    My All Social Web Project Started

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const port = 1023;
+
+app.use('/',require('./routes'));
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running the Server : ${err}`);
+    }
+    console.log(`Server is Up and Running on the Server ${port}`);
+});
