4840c395f184be826db71c34265e1a69a9db414f
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2023-02-17 15:06:19 +0530

    Starting cookies

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,13 +4,21 @@ module.exports.profile = function(req,res){
     });
 };
 
+// redner sign up page
 module.exports.signup = function(req,res){
     return res.render('user_sign_up',{
         title : "My Social | Sign up"
     });
 };
+
+// render sign in page
 module.exports.signin = function(req,res){
     return res.render('user_sign_in',{
         title : "My Social | Sign in"
     });
 };
+
+// get the sign up data
+module.exports.create = function(req,res){
+    return 
+}
